a6b55619e9c5ded4699d189d04cc70b081d5de82
Hemant Sherawat <hzs0084@auburn.edu>
2023-12-23 14:14:22 -0600

    JavaScript Integration commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(item => {
+    const questionButton = item.querySelector('.faq-question');
+    const answerContent = item.querySelector('.faq-answer');
+
+    questionButton.addEventListener('click', () => {
+        answerContent.classList.toggle('open'); /* Toggle the "open" class to show/hide the answer */
+    });
+});
