7e87d0d8fa770f14b4e31e79409125f3d83adf47
Hemant Sherawat <hzs0084@auburn.edu>
2023-12-27 07:39:22 -0600

    Add FAQ items and copy button functionality

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ faqItems.forEach(item => {
         answerContent.classList.toggle('open'); /* Toggle the "open" class to show/hide the answer */
     });
 });
+
+
+const copyButton = document.querySelector('#copy-button');
+const codeSnippet = document.querySelector('#code-snippet');
+
+copyButton.addEventListener('click', () => {
+  navigator.clipboard.writeText(codeSnippet.textContent)
+    .then(() => {
+      alert('Code copied to clipboard!'); // Optional success message
+    })
+    .catch(err => {
+      console.error('Failed to copy text: ', err);
+    });
+});
