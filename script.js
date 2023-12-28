893d51b5dab295c9a0d58a2ce269683604138476
Hemant Sherawat <hzs0084@auburn.edu>
2023-12-28 12:13:33 -0600

    Add code snippets and styling for terminal commands

--- script.js
+++ script.js
@@ -10,15 +10,4 @@ faqItems.forEach(item => {
 });
 
 
-const copyButton = document.querySelector('#copy-button');
-const codeSnippet = document.querySelector('#code-snippet');
-
-copyButton.addEventListener('click', () => {
-  navigator.clipboard.writeText(codeSnippet.textContent)
-    .then(() => {
-      alert('Code copied to clipboard!'); // Optional success message
-    })
-    .catch(err => {
-      console.error('Failed to copy text: ', err);
-    });
-});
+  
