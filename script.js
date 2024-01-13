3aef6b012bd66f659da765d306197502786d2ad3
Hemant Sherawat <hzs0084@auburn.edu>
2024-01-13 10:44:31 -0600

    Fix image resolution and formatting, add media query for smaller screens

--- script.js
+++ script.js
@@ -8,10 +8,3 @@ faqItems.forEach(item => {
         answerContent.classList.toggle('open'); /* Toggle the "open" class to show/hide the answer */
     });
 });
-
-// Add the following code to the existing JavaScript code
-// Select the copy icon inside the copy button
-const copyIcon = document.querySelector('.copy-button i');
-
-// Add a click event listener to the copy icon
-copyIcon.addEventListener('click', copyCode);
