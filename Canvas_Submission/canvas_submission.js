4247106e9e125cbdfc445af401b56cd76439e840
Hemant Sherawat <43252065+hzs0084@users.noreply.github.com>
2024-09-08 16:38:01 -0500

    Update canvas_submission.js

--- Canvas_Submission/canvas_submission.js
+++ Canvas_Submission/canvas_submission.js
@@ -3,10 +3,10 @@ const video = document.getElementById('instructional-video');
 const userAgent = navigator.userAgent;
 
 if (userAgent.indexOf('Mac OS X') !== -1) { 
-  video.src = 'Video Recordings/How to Compress files for Mac.mp4'; 
+  video.src = './How to compress for Mac Users.mp4'; 
 } else if (userAgent.indexOf('Windows') !== -1) {
-  video.src = 'Video Recordings/How to compress for Windows Users.mp4';
+  video.src = './How to compress for Windows Users.mp4';
 } else {
   // Default video or message if platform isn't detected
-  video.src = 'Video Recordings/How to compress for Windows Users.mp4'; 
+  video.src = './How to compress for Windows Users.mp4'; 
 }
