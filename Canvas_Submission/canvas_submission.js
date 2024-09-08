0128be208e6e3e043e9f607df0fc026ab5468c32
Hemant Sherawat <hzs0084@auburn.edu>
2024-09-08 13:50:35 -0500

    Added Video Recordings and Canvas Submission

---
+++ Canvas_Submission/canvas_submission.js
@@ -0,0 +1,11 @@
+const video = document.getElementById('instructional-video');
+const userAgent = navigator.userAgent;
+
+if (userAgent.indexOf('Mac OS X') !== -1) { 
+  video.src = 'How to Compress files for Mac.mp4'; 
+} else if (userAgent.indexOf('Windows') !== -1) {
+  video.src = 'How to compress for Windows Users.mp4';
+} else {
+  // Default video or message if platform isn't detected
+  video.src = 'How to compress for Windows Users.mp4'; 
+}
