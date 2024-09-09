609a596deb43446705b084b144e9ba72a5a2d028
Hemant Sherawat <43252065+hzs0084@users.noreply.github.com>
2024-09-09 14:44:51 -0500

    Update canvas_submission.js

--- Canvas_Submission/canvas_submission.js
+++ Canvas_Submission/canvas_submission.js
@@ -1,12 +1,12 @@
-// In canvas_submission.js
-const macVideoSection = document.querySelector('.mac-users');
-const windowsVideoSection = document.querySelector('.windows-users');
+// // In canvas_submission.js
+// const macVideoSection = document.querySelector('.mac-users');
+// const windowsVideoSection = document.querySelector('.windows-users');
 
-if (userAgent.indexOf('Mac OS X') !== -1) { 
-  macVideoSection.style.display = 'block'; 
-} else if (userAgent.indexOf('Windows') !== -1) {
-  windowsVideoSection.style.display = 'block';
-} else {
-  // Default: Show one of the sections 
-  windowsVideoSection.style.display = 'block'; 
-}
+// if (userAgent.indexOf('Mac OS X') !== -1) { 
+//   macVideoSection.style.display = 'block'; 
+// } else if (userAgent.indexOf('Windows') !== -1) {
+//   windowsVideoSection.style.display = 'block';
+// } else {
+//   // Default: Show one of the sections 
+//   windowsVideoSection.style.display = 'block'; 
+// }
